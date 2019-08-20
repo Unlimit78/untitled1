ea23fdc351055f5388da89130ac52dbc9691e27c
Unlimit78 <44531839+Unlimit78@users.noreply.github.com>
2019-08-20 15:58:02 +0300

    Update settings.py

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -144,6 +144,12 @@ CHANNEL_LAYERS = {
         },
     },
 }
+CACHES = {
+    "default": {
+         "BACKEND": "redis_cache.RedisCache",
+         "LOCATION": os.environ.get('REDIS_URL'),
+    }
+}
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
