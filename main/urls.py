8227bb5efb62309df5137915cbd784ce5ea2af93
Unlimit78 <44531839+Unlimit78@users.noreply.github.com>
2019-08-20 13:08:44 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import index,UserLogin,UserLogout,profile,ChangeInfo,\
-ChangePassword,RegisterUserView,RegisterDoneView,,DeleteUser
+ChangePassword,RegisterUserView,RegisterDoneView,DeleteUser
 
 
 urlpatterns = [
