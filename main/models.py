805b0ef1e191aa89895d8b33341c6239bf21ffae
Unlimit78 <44531839+Unlimit78@users.noreply.github.com>
2019-08-20 12:57:24 +0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.dispatch import Signal
-from .utilities import send_activation_notification
 
-user_registrated = Signal(providing_args=['instance'])
-def user_registrated_dispatcher(sender,**kwargs):
-    send_activation_notification(kwargs['instance'])
-user_registrated.connect(user_registrated_dispatcher)
 
 
 class AdvUser(AbstractUser):
