be7daf39dff5c202077811976fb8d59ac38ebc5d
Unlimit78 <44531839+Unlimit78@users.noreply.github.com>
2019-08-20 13:00:28 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import index,UserLogin,UserLogout,profile,ChangeInfo,\
-ChangePassword,RegisterUserView,RegisterDoneView,user_activate,DeleteUser
+ChangePassword,RegisterUserView,RegisterDoneView,,DeleteUser
 
 
 urlpatterns = [
@@ -12,7 +12,6 @@ urlpatterns = [
     path('accounts/profile/password/change/',ChangePassword.as_view(),name='password_change'),
     path('accounts/register/',RegisterUserView.as_view(),name='register'),
     path('accounts/register/done/',RegisterDoneView.as_view(),name='register_done'),
-    path('accounts/register/activate/<str:sign>/',user_activate,name='register_activate'),
     path('accounts/profile/delete/',DeleteUser.as_view(),name='profile_delete'),
 
 
