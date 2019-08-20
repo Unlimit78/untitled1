dd7009f6487bf5e6d2f1a03a2722c452b6cf253d
Unlimit78 <44531839+Unlimit78@users.noreply.github.com>
2019-08-20 17:45:56 +0300

    Delete aspi.py

--- untitled1/aspi.py
+++
@@ -1,5 +0,0 @@
-import os
-import django
-from channels.routing import get_default_applicationos.environ.setdefault(“DJANGO_SETTINGS_MODULE”, “untitled1.settings”)
-django.setup()
-application=get_default_application()
